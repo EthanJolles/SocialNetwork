33d62901ecc88c5265380fac34e397485ce014d9
Ethan Jolles <erjolles@gmail.com>
2022-01-22 01:35:45 -0500

    Fixing something

--- src/main/java/com/solvd/socialNetwork/controller/LoginController.java
+++ src/main/java/com/solvd/socialNetwork/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.solvd.socialNetwork.controller;
 
 
+import com.solvd.socialNetwork.SocialNetworkApplication;
 import com.solvd.socialNetwork.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class LoginController {
 
+    final static Logger LOGGER = LogManager.getLogger(LoginController.class);
+
     @PostMapping("login")
     public String handle(@RequestParam("username") String user, @RequestParam("password") String password) {
         if (UserService.validateUser(user, password)) {
+            LOGGER.info("Successful login attempt " + user + password);
             return "success";
         }
+        LOGGER.info("Failed login attempt " + user + password);
         return "failed";
     }
 
