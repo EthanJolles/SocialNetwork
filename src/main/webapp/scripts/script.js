8bc49a870da83ea9726cf237e7d2c83e0eaf0012
Ethan Jolles <erjolles@gmail.com>
2022-01-22 01:04:20 -0500

    Temporarily commenting out un-needed script

---
+++ src/main/webapp/scripts/script.js
@@ -0,0 +1,18 @@
+// const username = document.getElementById('name')
+// const password = document.getElementById('password')
+// const form = document.getElementById('form')
+// const errorElement = document.getElementById('error')
+//
+// const database_user = 'EthanJolles'
+// const database_password = 'solvdintern123'
+//
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if (username.value !== database_user || password.value !== database_password) {
+//         messages.push('Username or password is invalid')
+//     }
+//     if (messages.length > 0) {
+//         e.preventDefault()
+//         errorElement.innerText = messages.join(', ')
+//     }
+// })
