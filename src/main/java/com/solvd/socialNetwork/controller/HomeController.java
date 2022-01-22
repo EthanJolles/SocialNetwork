f4b0b269ed2f61ba91036f327d0e4613f2712a7e
Ethan Jolles <erjolles@gmail.com>
2022-01-22 01:02:06 -0500

    Home Controller that sets the homepage as index.html

--- src/main/java/com/solvd/socialNetwork/controller/HomeController.java
+++ src/main/java/com/solvd/socialNetwork/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.solvd.socialNetwork.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping
+    @GetMapping("/")
     public String homepage() {
-        return "Homepage";
+        return "index";
     }
 }
